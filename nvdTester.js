8926abd614b1b7726c9e05b459894274fc5757e1
Elias Serrano <eliasjosf26@gmail.com>
2024-11-08 18:50:31 -0600

    -D se eliminaron algunos fields de las responses, archivo de ejemplo para correr el servicio

--- nvdTester.js
+++ nvdTester.js
@@ -1,7 +1,7 @@
 const VulnerabilityScanner = require("./services/cve-nvd.service");
 
 //Los elements son los parametros que van a salir de la base de datos de assets digitales, siendo la marca y sistemas operativos principalmente 
-const elementsToScan = ['PAX','Apache', 'Azure', 'Microsoft', 'android']; 
+const elementsToScan = ['Apache Kylin']; 
 const scanner = new VulnerabilityScanner(elementsToScan);
 
 (async () => {
