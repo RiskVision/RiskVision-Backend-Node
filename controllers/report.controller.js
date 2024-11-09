ce315ec9bc016315db96a3c8a5525e2548ef0a46
Elias Serrano <eliasjosf26@gmail.com>
2024-11-08 23:48:19 -0600

    commit inicial para el endpoint master

--- controllers/report.controller.js
+++ controllers/report.controller.js
@@ -6,15 +6,30 @@ const ReportGenerator = require('../utils/reportGenerator'); // Adjust path as n
 
 exports.getReport = catchAsync(async (req, res, next) => {
     try {
+
+        //Extraer los datos de los activos digitales enfocarse en la marca y Sistema Operativo
+        var elementsToScan =[];
+
         // Obtain vulnerability data
-        const vulnerability = await nvdService.getNVDResponse();
+        const scanner = new VulnerabilityScanner(elementsToScan);
+
+        (async () => {
+            const results = await scanner.start();
+            const resultsString = JSON.stringify(results, null, 2); // Convertir los resultados a una cadena JSON con formato
+        })();
+
+        console.log(resultsString);
 
         // Generate AI response content
-        const aiContent = await aiService.generateAIResponse(vulnerability);
+        //const aiContent = await aiService.generateAIResponse(vulnerability);
 
         // Generate Word report with the retrieved content
         const reportGenerator = new ReportGenerator();
         const wordBuffer = await reportGenerator.createReport(aiContent);
+
+        //Guardar reporte en el blob storage de Azure (habrá que cambiar el report generator)
+
+
 
         // Set headers and send the file as a response
         res.setHeader(
