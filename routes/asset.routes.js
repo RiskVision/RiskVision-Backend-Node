faa4967713989b29dfcae2cc4f9f43bc47427507
DiegoRdzD <91581962+DiegoRdzD@users.noreply.github.com>
2024-11-22 18:24:37 -0600

    Cambio de tabla base de datos

--- routes/asset.routes.js
+++ routes/asset.routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
         const pool = await poolPromise;
         const result = await pool.request()
             .query(`
-                SELECT * FROM dbo.TestTable
+                SELECT * FROM dbo.asset_Inventory
             `);
         res.json(result.recordset);
     } catch (err) {
@@ -24,7 +24,7 @@ router.get('/:id_activo', async (req, res) => {
         const result = await pool.request()
             .input('id_activo', sql.VarChar, id_activo)
             .query(`
-                SELECT * FROM dbo.TestTable 
+                SELECT * FROM dbo.asset_Inventory
                 WHERE id_activo = @id_activo
             `);
         
@@ -66,7 +66,7 @@ router.post('/', async (req, res) => {
             .input('frecuencia_monitoreo', sql.VarChar, frecuencia_monitoreo)
             .input('monitoreo_seguridad', sql.VarChar, monitoreo_seguridad)
             .input('auditoria_acceso', sql.VarChar, auditoria_acceso)
-            .query('INSERT INTO dbo.TestTable (id_activo, nombre_activo, descripcion, marca, modelo, cantidad, sistema_operativo, version_os, usuario_responsable, equipo_soporte, ubicacion, servidor_deployment, fecha_adquisicion, fecha_ultima_actualizacion, direccion_mac, direccion_ip, nivel_criticidad, clasificacion_activo, estado, plan_recuperacion_drp, frecuencia_monitoreo, monitoreo_seguridad, auditoria_acceso) VALUES (@id_activo, @nombre_activo, @descripcion, @marca, @modelo, @cantidad, @sistema_operativo, @version_os, @usuario_responsable, @equipo_soporte, @ubicacion, @servidor_deployment, @fecha_adquisicion, @fecha_ultima_actualizacion, @direccion_mac, @direccion_ip, @nivel_criticidad, @clasificacion_activo, @estado, @plan_recuperacion_drp, @frecuencia_monitoreo, @monitoreo_seguridad, @auditoria_acceso)');
+            .query('INSERT INTO dbo.asset_Inventory (id_activo, nombre_activo, descripcion, marca, modelo, cantidad, sistema_operativo, version_os, usuario_responsable, equipo_soporte, ubicacion, servidor_deployment, fecha_adquisicion, fecha_ultima_actualizacion, direccion_mac, direccion_ip, nivel_criticidad, clasificacion_activo, estado, plan_recuperacion_drp, frecuencia_monitoreo, monitoreo_seguridad, auditoria_acceso) VALUES (@id_activo, @nombre_activo, @descripcion, @marca, @modelo, @cantidad, @sistema_operativo, @version_os, @usuario_responsable, @equipo_soporte, @ubicacion, @servidor_deployment, @fecha_adquisicion, @fecha_ultima_actualizacion, @direccion_mac, @direccion_ip, @nivel_criticidad, @clasificacion_activo, @estado, @plan_recuperacion_drp, @frecuencia_monitoreo, @monitoreo_seguridad, @auditoria_acceso)');
         res.status(201).send('Activo creado');
     } catch (err) {
         res.status(500).send('Error al crear el activo');
@@ -103,7 +103,7 @@ router.put('/:id_activo', async (req, res) => {
             .input('frecuencia_monitoreo', sql.VarChar, frecuencia_monitoreo)
             .input('monitoreo_seguridad', sql.VarChar, monitoreo_seguridad)
             .input('auditoria_acceso', sql.VarChar, auditoria_acceso)
-            .query('UPDATE dbo.TestTable SET id_activo = @id_activo, nombre_activo = @nombre_activo, descripcion = @descripcion, marca = @marca, modelo = @modelo, cantidad = @cantidad, sistema_operativo = @sistema_operativo, version_os = @version_os, usuario_responsable = @usuario_responsable, equipo_soporte = @equipo_soporte, ubicacion = @ubicacion, servidor_deployment = @servidor_deployment, fecha_adquisicion = @fecha_adquisicion, fecha_ultima_actualizacion = @fecha_ultima_actualizacion, direccion_mac = @direccion_mac, direccion_ip = @direccion_ip, nivel_criticidad = @nivel_criticidad, clasificacion_activo = @clasificacion_activo, estado = @estado, plan_recuperacion_drp = @plan_recuperacion_drp, frecuencia_monitoreo = @frecuencia_monitoreo, monitoreo_seguridad = @monitoreo_seguridad, auditoria_acceso = @auditoria_acceso WHERE id_activo = @id_activo');
+            .query('UPDATE dbo.asset_Inventory SET id_activo = @id_activo, nombre_activo = @nombre_activo, descripcion = @descripcion, marca = @marca, modelo = @modelo, cantidad = @cantidad, sistema_operativo = @sistema_operativo, version_os = @version_os, usuario_responsable = @usuario_responsable, equipo_soporte = @equipo_soporte, ubicacion = @ubicacion, servidor_deployment = @servidor_deployment, fecha_adquisicion = @fecha_adquisicion, fecha_ultima_actualizacion = @fecha_ultima_actualizacion, direccion_mac = @direccion_mac, direccion_ip = @direccion_ip, nivel_criticidad = @nivel_criticidad, clasificacion_activo = @clasificacion_activo, estado = @estado, plan_recuperacion_drp = @plan_recuperacion_drp, frecuencia_monitoreo = @frecuencia_monitoreo, monitoreo_seguridad = @monitoreo_seguridad, auditoria_acceso = @auditoria_acceso WHERE id_activo = @id_activo');
         res.send('Activo actualizado');
     } catch (err) {
         res.status(500).send('Error al actualizar el activo');
@@ -115,7 +115,7 @@ router.delete('/:id_activo', async (req, res) => {
     try {
         const pool = await poolPromise;
         const { id_activo } = req.params;
-        await pool.request().input('id_activo', sql.VarChar, id_activo).query('DELETE FROM dbo.TestTable WHERE id_activo = @id_activo');
+        await pool.request().input('id_activo', sql.VarChar, id_activo).query('DELETE FROM dbo.asset_Inventory WHERE id_activo = @id_activo');
         res.send('Activo eliminado');
     } catch (err) {
         res.status(500).send('Error al eliminar el activo');
